b4c12f02217003c4a4d0c9c81e974409b3590edf
Mats Julius Funke <125814808+matsjfunke@users.noreply.github.com>
2024-04-19 02:09:00 +0200

    just being a functional bro

--- main.py
+++ main.py
@@ -4,15 +4,17 @@ matsjfunke
 import ollama
 
 
-def load_and_chunk_file(file_name):
+def load_file(file_name):
     # Open the file
     with open(file_name, encoding="utf-8-sig") as f:
         # Read the entire content of the file
         content = f.read()
+    return content
 
+
+def chunk_text(content):
     # Split the content into blocks of text separated by empty lines
     text_blocks = [block.strip() for block in content.split('\n\n') if block.strip()]
-
     return text_blocks
 
 
@@ -20,15 +22,18 @@ def create_embeddings(embeddings_model, chunks):
     embeddings = []
     for chunk in chunks:
         embeddings.append(ollama.embeddings(model=embeddings_model, prompt=chunk))
-
     return embeddings
 
 
 def main():
     file_name = "peter-pan.txt"  # input("enter file name: ")
 
-    chunks = load_and_chunk_file(file_name)
+    file_content = load_file(file_name)
+
+    chunks = chunk_text(file_content)
+
     embeddings = create_embeddings('nomic-embed-text', chunks)
+
     print(len(embeddings))
 
 
