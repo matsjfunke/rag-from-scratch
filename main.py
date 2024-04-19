89b4e9f5285ccc176f952158539a4e8ffb4f6fae
Mats Julius Funke <125814808+matsjfunke@users.noreply.github.com>
2024-04-19 04:01:39 +0200

    embedding creation, saving and loading works

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ matsjfunke
 import ollama
 import json
 import os
+import time
 
 
 def load_file(file_name):
@@ -20,24 +21,55 @@ def chunk_text(content):
     return text_blocks
 
 
-def create_embeddings(embeddings_model, chunks):
-    embeddings = []
-    for chunk in chunks:
-        embeddings.append(ollama.embeddings(model=embeddings_model, prompt=chunk))
-    return embeddings
-
-
 def save_embeddings(file_name, embeddings):
     # make folder
     folder_name = "embeddings"
     if not os.path.exists(folder_name):
         os.makedirs(folder_name)
 
-    file_path = os.path.join(folder_name, file_name)
+    # save embeddings as json
+    file_path = os.path.join(folder_name, file_name[:-3] + "json")
     if not os.path.exists(file_path):
         with open(file_path, 'w') as json_file:
             json.dump(embeddings, json_file)
         print("Data saved to", file_path)
+    else:
+        # double check for loaded_embeddings TDOO: maybe delete this
+        print(f"Embeddings from {file_name} are already saved")
+
+
+def load_embeddings_from_json(file_name):
+    file_path = os.path.join("embeddings", file_name[:-3] + "json")
+
+    # Check if the file exists
+    if os.path.exists(file_path):
+        # Read the data from the JSON file
+        with open(file_path, 'r') as json_file:
+            loaded_embeddings = json.load(json_file)
+        print("Data loaded embedding successfully.")
+        return loaded_embeddings
+    else:
+        print("No embeddings saved")
+        return False
+
+
+def create_embeddings(embeddings_model, chunks):
+    print("creating embeddings")
+    start_time = time.perf_counter()
+    embeddings = []
+    for chunk in chunks:
+        embeddings.append(ollama.embeddings(model=embeddings_model, prompt=chunk))
+    print(f"Time to create embeddings: {time.perf_counter() - start_time}s")
+    return embeddings
+
+
+def handle_embeddings(embeddings_model, chunks, file_name):
+    embeddings = load_embeddings_from_json(file_name)
+    if embeddings is False:
+        embeddings = create_embeddings(embeddings_model, chunks)
+        return save_embeddings(file_name, embeddings)
+    else:
+        return embeddings
 
 
 def main():
@@ -47,9 +79,7 @@ def main():
 
     chunks = chunk_text(file_content)
 
-    embeddings = create_embeddings('nomic-embed-text', chunks)
-    save_embeddings(file_name, embeddings)
-
+    embeddings = handle_embeddings('nomic-embed-text', chunks, file_name)
 
 
 if __name__ == "__main__":
